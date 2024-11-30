70ed894a348fa1e8e060b9f0d0ce19214b0c75c8
MUHAMMED WASEEM VILLAN <60029472+Waseem27-art@users.noreply.github.com>
2024-12-01 00:46:38 +0100

    Update run_benchmark.py
    changed each time to repeat the attack on different models and datasets

--- tasks/run_benchmark.py
+++ tasks/run_benchmark.py
@@ -6,13 +6,14 @@ def run():
 
     clean_acc, robust_acc = benchmark(
         dataset="URL",
-        model="STG_Default",
+        model="STG_madry",
         distance="L2",
         constraints=True,
     )
+    print("LCLD on STG madry(adversary)")
     print(f"Clean accuracy: {clean_acc}")
     print(f"Robust accuracy: {robust_acc}")
-
+        
 
 if __name__ == "__main__":
     run()
